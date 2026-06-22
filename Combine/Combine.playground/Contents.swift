07f91255e52a89a5ac93b7c231d48c804f29889b
Museer Ansari <museeransari786@gmail.com>
2026-06-23 01:10:18 +0530

    Initial commit

--- Combine/Combine.playground/Contents.swift
+++ Combine/Combine.playground/Contents.swift
@@ -1,76 +1,53 @@
-import UIKit
+import Foundation
 import Combine
 
-// ******** Just Publisher ************
-/*
- Emits a single value immediately and then finishes.
- */
-//Just("Hello")
-//    .sink { value in
-//        print(value)
-//    }
+var anyCancellable = Set<AnyCancellable>()
 
-// *************** Future publisher ************
-/*
- Produces a single value asynchronously (or an error).
- */
-var cancellable = Set<AnyCancellable>()
-//
-//Future<Int, Error> { promise in
-//    DispatchQueue.main.async {
-//        promise(.success(2))
-//    }
-//}
-//.map({$0 * 2})
-//.sink { value in
-//    print("value === \(value)")
-//} receiveValue: { print($0)
-//}
-//.store(in: &cancellable)
-//
-////future.self
-//
-//
-//Empty<Int, URLError>()
-//    .sink(receiveCompletion: { print("Done:", $0) },
-//          receiveValue: { print($0) })
-
-//Fail<String, URLError>(error: URLError(.badURL))
-//    .sink { error in
-//        print(error)
-//    } receiveValue: { value in
-//        print("value == \(value)")
-//    }
-
-//Fail<String, URLError>(outputType: String.self, failure: URLError(.badURL))
-//    .sink { eror in
-//        print("error === \(eror)")
-//    } receiveValue: { value in
-//        print("my name is museer")
-//    }
-
-//let subject = CurrentValueSubject<String, Never>("sahil")
-//subject.sink { value in
-//    print(value)
-//}.store(in: &cancellable)
-//
-//subject.send("Hello")
-//subject.send("Museer ansari")
-//subject.sink { value in
-//    print(value)
-//}.store(in: &cancellable)
-//
-//subject.sink { value in
-//    print(value)
-//}.store(in: &cancellable)
-
-let currentValueSubject = CurrentValueSubject<Int, Never>(0)
-currentValueSubject.sink { value in
-    print("CurrentValueSubject received: \(value)")
+struct Employee: Decodable {
+    let id: String
 }
-//currentValueSubject.send(1)
-//currentValueSubject.value = 2
-// Output:
-// CurrentValueSubject received: 0
-// CurrentValueSubject received: 1
-// CurrentValueSubject received: 2
+
+struct Address: Decodable {
+    let landmark: String
+}
+
+func fetchEmploye() -> AnyPublisher<Employee, Error> {
+    URLSession.shared.dataTaskPublisher(for: URL(string: "https://www.com/employe")!)
+        .map(\.data)
+        .decode(type: Employee.self, decoder: JSONDecoder())
+        .mapError({$0})
+        .eraseToAnyPublisher()
+}
+
+func fetchAddress() -> AnyPublisher<Address, Error> {
+    URLSession.shared.dataTaskPublisher(for: URL(string: "https://www.com/employe/adrress")!)
+        .map(\.data)
+        .decode(type: Address.self, decoder: JSONDecoder())
+        .mapError({$0})
+        .eraseToAnyPublisher()
+}
+
+@MainActor
+func fetchData() {
+    fetchEmploye()
+        .zip(fetchAddress()) //
+        .receive(on: DispatchQueue.main)
+        .sink { error in
+            print("error == \(error)")
+        } receiveValue: { (employee, address) in
+            print(employee.id)
+            print(address.landmark)
+        }.store(in: &anyCancellable)
+    
+    fetchAddress()
+        .combineLatest(fetchEmploye())
+        .sink(receiveCompletion: {print("\($0)")},
+              receiveValue: { (address, employe) in
+            print(address.landmark)
+        })
+        
+    
+    
+}
+
+
--- Concurrency/Concurrency.playground/Pages/TaskGroup.xcplaygroundpage/Contents.swift
+++ Concurrency/Concurrency.playground/Pages/TaskGroup.xcplaygroundpage/Contents.swift
@@ -1,7 +1,29 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+struct User: Codable {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
 
-//: [Next](@next)
+func fetchMultipleUser() async throws -> [User] {
+    
+    let userIds = [1,2,3,4,5,6,7,8]
+    return try await withThrowingTaskGroup { group in
+        for id in userIds {
+            group.addTask {
+                let url = URL(string: "http://www.google.com/user")!
+                let (data, _) = try await URLSession.shared.data(from: url)
+                return try JSONDecoder().decode(User.self, from: data)
+            }
+        }
+        
+        // Collect users
+        var users: [User] = []
+        for try await user in group {
+            users.append(user)
+        }
+        return users
+    }
+}
